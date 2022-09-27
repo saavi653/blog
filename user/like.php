4cba14c902b97c5c5d8c7a361370085f49e15662
savita <saavi653@gmail.com>
2022-09-27 12:17:41 +0530

    changes done in like.php

--- user/like.php
+++ user/like.php
@@ -1,47 +1,40 @@
 <?php
 session_start();
-
-    if(isset($_GET['id']))
+include('../admin/conn.php');
+if(isset($_GET['id']))
+{
+    $id=$_GET['id'];
+    $user_id=$_GET['user_id'];
+    $obj=new connection();
+    $obj=$obj->conn();
+    if($_GET['likes']==1)
     {
-        $id=$_GET['id'];
-        $user_id=$_GET['user_id'];
-        $obj=new pdo("mysql:host=localhost;dbname=blog;","root","");
-        $fun_data=$obj->query("select *from fun");
-        $fetch_fun=$fun_data->fetchAll(pdo::FETCH_ASSOC);
-        if($_GET['likes']==1)
-        {
-            $count=0;
-            foreach($fetch_fun as $k =>$v)
-            { 
-                if($id==$v['blog_id']&& $user_id==$v['user_id'])
-                { 
-                    $obj->exec("update fun set likes='1',dislikes='0' where blog_id='$id' and user_id= '$user_id' ");
-                    header('location:userpage.php');  
-                    $count++; 
-                }
-            }
-            if($count==0)
-            {
-                $obj->query("insert into fun(blog_id,user_id,likes,dislikes)values('$id','$user_id',1,0)");
-                header('location:userpage.php'); 
-            }    
-        }    
+        $data=$obj->query("select likes from fun where blog_id='$id' and user_id='$user_id'");
+        $fun=$data->fetchAll(pdo::FETCH_ASSOC);
+        if(!empty($fun))
+        { 
+            $obj->exec("update fun set likes='1',dislikes='0' where blog_id='$id' and user_id= '$user_id' ");
+            header('location:userpage.php');  
+        }
         else
-        {   
-            $count=0;
-            foreach($fetch_fun as $k =>$v)
-            { 
-                if($id==$v['blog_id']&& $user_id==$v['user_id'])
-                { 
-                    $obj->exec("update fun set likes='0',dislikes='1'where blog_id='$id' and user_id='$user_id' ");
-                    header('location:userpage.php');  
-                    $count++; 
-                }
-            }
-            if($count==0)
-            {
-                $obj->query("insert into fun(blog_id,user_id,likes,dislikes)values('$id','$user_id',0,1)");
-                header('location:userpage.php'); 
-            }        
-        } 
-    }
+        {
+            $obj->query("insert into fun(blog_id,user_id,likes,dislikes)values('$id','$user_id',1,0)");
+            header('location:userpage.php'); 
+        }
+    }    
+    else
+    {
+        $data=$obj->query("select likes from fun where blog_id='$id' and user_id='$user_id'");
+        $fun=$data->fetchAll(pdo::FETCH_ASSOC);
+        if(!empty($fun))
+        { 
+            $obj->exec("update fun set likes='0',dislikes='1' where blog_id='$id' and user_id= '$user_id' ");
+            header('location:userpage.php');  
+        }
+        else
+        {
+            $obj->query("insert into fun(blog_id,user_id,likes,dislikes)values('$id','$user_id',0,1)");
+            header('location:userpage.php'); 
+        }
+    }   
+}  
